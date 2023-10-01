d3af4723b5b93166b17cbc8949706ecd0d24eb64
Isaac Liu <liuisaac05@gmail.com>
2023-09-30 23:27:45 -0700

    Progress Bar testing + minor changes modified:   src/components/Constant/ProgressBar.tsx

--- src/components/Constant/ProgressBar.tsx
+++ src/components/Constant/ProgressBar.tsx
@@ -3,7 +3,7 @@ const ProgressBar = () => {
   return (
     <div className="fixed bottom-0 bg-opacity-100 h-2 w-full text-white z-10 backdrop-blur-sm mb-1">
         {/* Bar */}
-        <div className="h-full w-[100vh] bg-white z-20 opacity-40 rounded-r-full" ></div>
+        <div className="h-full w- bg-white z-20 opacity-40 rounded-r-full" ></div>
     </div>
   )
 }
