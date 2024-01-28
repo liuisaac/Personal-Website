88eb7a7d6e8c046e00e31596aebd66b2c4761a21
Isaac Liu <liuisaac05@gmail.com>
2024-01-28 03:02:48 -0800

    ok fine vercel

--- src/components/Home/Three/BoatWaves.tsx
+++ src/components/Home/Three/BoatWaves.tsx
@@ -12,29 +12,29 @@ const rippleFactor = 0.07; // wave travel distance // HIGHER = less travel, LOWE
 let a = 3; // variable amplitude // isolated amplitude control
 const count = 25;
 
-const vertexShader = `
-  attribute float scale;
+// const vertexShader = `
+//   attribute float scale;
   
-  void main() {
-    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+//   void main() {
+//     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
     
-    gl_PointSize = scale * (300.0 / -mvPosition.z);
-    gl_Position = projectionMatrix * mvPosition;
-  }
-`;
+//     gl_PointSize = scale * (300.0 / -mvPosition.z);
+//     gl_Position = projectionMatrix * mvPosition;
+//   }
+// `;
 
-const fragmentShader = `
-  uniform vec3 color;
+// const fragmentShader = `
+//   uniform vec3 color;
   
-  void main() {
-    if (length(gl_PointCoord - vec2(0.5, 0.5)) > 0.475) discard;
+//   void main() {
+//     if (length(gl_PointCoord - vec2(0.5, 0.5)) > 0.475) discard;
     
-    gl_FragColor = vec4(color, 1.0);
-  }
-`;
+//     gl_FragColor = vec4(color, 1.0);
+//   }
+// `;
 
 function Points() {
-    const imgTex = useLoader(THREE.TextureLoader, wavepoint);
+    // const imgTex = useLoader(THREE.TextureLoader, wavepoint);
     const positionBufferRef = useRef<THREE.BufferAttribute | null>();
     const scaleBufferRef = useRef<THREE.BufferAttribute | null>();
 
