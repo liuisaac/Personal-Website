992f5b98a27dae6982954ca10015ef039c6e1bc9
Isaac Liu <liuisaac05@gmail.com>
2024-01-30 03:04:58 -0800

    fixed mobile enough 	modified:   src/components/Home/Projects/Projects.tsx

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -5,13 +5,13 @@ const Projects = () => {
     return (
         <div>
             <img src={project} className="ml-8 h-12 my-16 z-20" />
-            <div className="grid grid-flow-row-dense grid-cols-3 w-3/5 mx-auto gap-5">
+            <div className="grid grid-flow-row-dense sm:grid-cols-3 grid-cols-1 w-3/5 mx-auto gap-5">
                 {proj.map((bobble) => (
                     <div
                         key={bobble.id}
                         className={`${
-                            bobble.ploppiness ? "col-span-2 row-span-2" : "col-span-1 row-span-1"
-                        } bg-[#201F23] sm:flex flex-col hidden w-full mx-auto mt-12 rounded-xl h-full overflow-hidden`}
+                            bobble.ploppiness ? "sm:col-span-2 col-span-1 sm:row-span-2 row-span-1" : "col-span-1 row-span-1"
+                        } bg-[#201F23] flex flex-col w-full mx-auto mt-12 rounded-xl h-full overflow-hidden`}
                     >
                         <img src={bobble.thumbnail} className="hover:scale-105 transition duration-400 ease-in-out" />
                         <span className=" inline font-semibold font-roboto text-white tracking-wider py-1 text-xl ">
